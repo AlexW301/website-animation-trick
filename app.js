79235966acdfa30714dfec4f43d8d9a9b0a21f92
Alex Waller <alex@pmlhomeloans.com>
2022-04-13 09:32:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
 
 tl.to('.text', {y: '0%', duration: 1, stagger: 0.25});
-tl.to('.slider', {y: '-100%', duration: 1.5})
+tl.to('.slider', {y: '-100%', duration: 1.5, delay: 0.5});
+// "-=1" makes it so these two transitions start around the same time
+tl.to('.intro', {y: "-100%", deration: 1}, "-=1");
+tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1})
+tl.fromTo('.big-text', {opacity: 0}, {opacity: 1, duration: 1}, "-=1")
